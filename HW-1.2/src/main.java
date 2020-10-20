d659925beb065e375372e8402eea3479d462dd93
Derya Bel <56176912+deryabel@users.noreply.github.com>
2020-10-20 11:19:17 +0300

    Add files via upload

---
+++ HW-1.2/src/main.java
@@ -0,0 +1,21 @@
+
+public class main {
+	public static void main (String[] args) {
+		int speed=70;
+		int time1=6;
+		int time2=10;
+		int time3=15;
+		int distance1=speed*time1;
+		int distance2=speed*time2;
+		int distance3=speed*time3;
+		System.out.println("•The distance the car will travel in 6 hours="+distance1);
+		System.out.println("•The distance the car will travel in 10 hours="+distance2);
+		System.out.println("•The distance the car will travel in 15 hours="+distance3);
+		
+		
+		
+	
+		
+	}
+
+}
